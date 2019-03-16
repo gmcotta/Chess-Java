e69bd274af75572efe9fbea540f684bb4b5bed2c
Gustavo Cotta <gmcotta34@gmail.com>
2019-03-16 12:11:01 -0300

    ChessException and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+//import boardgame.Board;
+//import boardgame.Position;
 import chess.ChessMatch;
 
 public class Program {
